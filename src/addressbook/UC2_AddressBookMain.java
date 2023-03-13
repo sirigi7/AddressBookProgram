b199aff82ffee7e260100325eff31611225b9171
sandeep <sirigi.reddy7@gmail.com>
2023-03-13 22:59:08 +0530

    Added: WelcomeTo Address Book

---
+++ src/addressbook/UC2_AddressBookMain.java
@@ -0,0 +1,18 @@
+package addressbook;
+
+public class UC2_AddressBookMain {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Address Book Program");
+        AddressBook addressBook1 = new AddressBook();
+        addressBook1.addContact();
+        System.out.println(addressBook1.contact.getFirstName());
+        System.out.println(addressBook1.contact.getLastName());
+        System.out.println(addressBook1.contact.getAddress());
+        System.out.println(addressBook1.contact.getCity());
+        System.out.println(addressBook1.contact.getState());
+        System.out.println(addressBook1.contact.getZip());
+        System.out.println(addressBook1.contact.getPhoneNumber());
+        System.out.println(addressBook1.contact.getEmailId());
+    }
+}
